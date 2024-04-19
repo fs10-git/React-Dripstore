6660f408b0691dab3c50a520d039608094bf7f2b
edivalter <junioredivalter25@gmail.com>
2024-04-19 09:53:21 -0300

    estilizações-ao-carrinho-e-product

--- src/Components/CardLists/Card.jsx
+++ src/Components/CardLists/Card.jsx
@@ -1,15 +1,20 @@
 import "./Card.css";
 import kSwiss from "/images/kSwiss.png";
 
-export default function Card(){
-    return(
-        <>
-            <div class="trendingProduct" tabIndex={0}>
-                <div class="productImage"><span>30% off</span><img src={kSwiss} alt="sneaker kswiss" /></div>
-                <b>tênis</b>
-                <h4>K-Swiss V8 - Masculino</h4>
-                <p><span>$200</span> $100</p>
-            </div>
-        </>
-    )
+export default function Card() {
+  return (
+    <>
+      <div className="trendingProduct" tabIndex={0}>
+        <div className="productImage">
+          <span>30% off</span>
+          <img src={kSwiss} alt="sneaker kswiss" />
+        </div>
+        <b>tênis</b>
+        <h4>K-Swiss V8 - Masculino</h4>
+        <p>
+          <span>$200</span> $100
+        </p>
+      </div>
+    </>
+  );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import "./Header.css";
 import Vector from "/images/Vector.png";
+
 // import Link from "react-router-dom";
 
 export default function Header() {
