dee74a131bf7cbaf6e72d109f52d057fcd5a0928
edivalter <junioredivalter25@gmail.com>
2024-04-26 11:23:24 -0300

    pagina_de_finalizar_compras_concluida

--- src/Components/FinalizarCompras/FinalizarCompras.jsx
+++ src/Components/FinalizarCompras/FinalizarCompras.jsx
@@ -48,8 +48,8 @@ export default function Finalizar() {
             <button className="botaoo">Realizar Pagamento</button>
           </div>
         </div>
-        <div>
-          <h3 className="title_a">Informações de Entrega</h3>
+        <div className="lado_a2">
+          <h3 className="title_a2">Informações de Entrega</h3>
           <p>Endereço *</p>
           <input type="text" placeholder="Insira seu endereço" />
           <p>Bairro *</p>
@@ -60,6 +60,53 @@ export default function Finalizar() {
           <input type="number" placeholder="Insira seu CEP" />
           <p>Complemento *</p>
           <input type="text" placeholder="Insira seu complemento" />
+        </div>
+        <div className="lado_a3">
+          <h3 className="title_a3">Informações de Pagamento</h3>
+          <h4 className="subtitle">Forma de Pagamento</h4>
+          <div className="input_radio">
+            <section className="radio_a">
+              <input
+                className="radio"
+                type="radio"
+                name="opcao"
+                id="cartao"
+                checked
+              />
+              <label htmlFor="cartao">Cartão de Crédito</label>
+            </section>
+            <section className="radio_b">
+              <input type="radio" name="opcao" id="boleto" />
+              <label htmlFor="boleto">Boleto</label>
+            </section>
+          </div>
+          <p>Nome do Cartão *</p>
+          <input
+            className="input_a"
+            type="text"
+            placeholder="Insira o nome do Cartão"
+          />
+          <div className="info_cartao">
+            <div>
+              <p>Número do Cartão *</p>
+              <input type="number" placeholder="Insira o número do Cartão" />
+            </div>
+            <div>
+              <p>Data de validade do Cartão *</p>
+              <input type="number" placeholder="Insira a validade do Cartão" />
+            </div>
+          </div>
+          <p>CVV *</p>
+          <input className="CVV" type="number" placeholder="CVV*" />
+        </div>
+        <div className="lado_a4">
+          <h4>Finalizar Compra</h4>
+          <div className="total">
+            <p>Total</p>
+            <p className="preco_final">R$ 219,00</p>
+          </div>
+          <p className="valor">ou 10x de R$ 21,00 sem juros</p>
+          <button className="botaoo">Realizar Pagamento</button>
         </div>
       </div>
     </>
