22899d736366946fa0ea350b854f1415e2d59b55
EliasFernandes03 <elias.neto@dunasystem.com>
2024-07-25 16:03:16 -0400

    cards

--- src/Components/CardLists/Card.jsx
+++ src/Components/CardLists/Card.jsx
@@ -1,14 +1,15 @@
+import React from "react";
 import "./Card.css";
 import kSwiss from "/images/kSwiss.png";
 
-export default function Card(){
+export default function Card({produto}){
     return(
         <>
             <div class="trendingProduct" tabIndex={0}>
-                <div class="productImage"><span>30% off</span><img src={kSwiss} alt="sneaker kswiss" /></div>
-                <b>tênis</b>
-                <h4>K-Swiss V8 - Masculino</h4>
-                <p><span>$200</span> $100</p>
+                <div class="productImage"><span>{produto.desconto*100}% off</span><img src={kSwiss} alt="sneaker kswiss" /></div>
+                <b>{produto.categoria}</b>
+                <h4>{produto.modelo}</h4>
+                <p><span>${produto.valor_total}</span> ${produto.valor_total*( 1 - produto.desconto).toFixed(2)}</p>
             </div>
         </>
     )
--- src/Components/CardLists/HomeTrendingCardList.jsx
+++ src/Components/CardLists/HomeTrendingCardList.jsx
@@ -1,17 +1,37 @@
 import "./CardList.css";
 import Card from "./Card";
+import axios from "axios"
+import { useState,useEffect } from "react";
 
 export default function HomeCardList(){
-    let homeCardListArray = [];
-    for(var index=0; index<8; index++){
-        homeCardListArray.push(<Card key={index} /> )
-    }
 
+ const [produtos, setProdutos] = useState([])
+
+
+ useEffect(() => {
+    async function fetchData() {
+        try {
+            const produtos = await axios.get("http://localhost:666/produtos-alta/getAll")
+            setProdutos(produtos.data)
+        } catch(error){
+            console.log(`Error ao buscar dados ${error}`)
+        }
+    }
+    fetchData()
+ },[])
     return(
         <>
             <div id="trending">
                 <div><h3>Produtos em alta</h3><a href="#">Ver todos <i class="fa-solid fa-arrow-right"></i></a></div>
-                <div id="trendingContent">{homeCardListArray}</div>
+                <div id="trendingContent">
+                    {produtos.map((produto) => (
+                             <Card 
+                                key={produto.id}
+                                produto={produto}
+                             />
+                    ))}
+
+                </div>
             </div>
         </>
     )
